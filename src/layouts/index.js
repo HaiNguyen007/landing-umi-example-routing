276f8853095da4a07bbe8a0ae7280aab4e325061
Hai Nguyen <hai@ahijsc.com>
2021-08-15 18:47:00 +0700

    layouts/index.js using class component props

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,53 +1,42 @@
-import React, { useEffect, useState, useRef } from "react";
-import { withRouter, Switch } from "umi";
-import { enquireScreen } from "enquire-js";
-import { TweenOneGroup } from "rc-tween-one";
-import Header from "./Nav0";
-import Footer from "./Footer0";
+import React, { Component } from 'react';
+import { enquireScreen } from 'enquire-js';
+import Header from './Nav0';
+import Footer from './Footer0';
 
-import { Nav00DataSource, Footer00DataSource } from "./data.source.js";
+import {
+  Nav00DataSource,
+  Footer00DataSource,
+} from './data.source.js';
 
 let isMobile;
 enquireScreen((b) => {
   isMobile = b;
 });
 
-const animate = {
-  enter: { x: "100vw", duration: 1000, ease: "easeInOutCubic" },
-  leave: { x: "-30vw", duration: 1000, ease: "easeInOutCubic" },
-};
+class Layout extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isMobile,
+    };
+  }
 
-export default withRouter(({ children, location }) => {
-  const [mobile, setIsMobile] = useState(isMobile);
-  useEffect(() => {
+  componentDidMount() {
+    // 适配手机屏幕;
     enquireScreen((b) => {
-      setIsMobile(!!b);
+      this.setState({ isMobile: !!b });
     });
-  }, []);
-  const { pathname } = location;
-  const pathArray = pathname.split(".")[0].split("/");
-  const path = pathArray[pathArray.length - 1];
-  const key = path || "index";
-  const enter = key === "index" ? animate.leave : animate.enter;
-  const leave = key === "index" ? animate.enter : animate.leave;
+  }
 
-  return (
-    <div>
-      <Header dataSource={Nav00DataSource} isMobile={mobile} />
-      <TweenOneGroup
-        enter={[
-          { type: "set", zIndex: key === "index" ? 0 : 1 },
-          { ...enter, type: "from" },
-        ]}
-        leave={[{ type: "set", zIndex: key === "index" ? 1 : 0 }, leave]}
-        appear={false}
-        className="app-layout"
-      >
-        <div key={key} className="layout-anim-child">
-          <Switch location={location}>{children.props.children}</Switch>
-        </div>
-      </TweenOneGroup>
-      <Footer dataSource={Footer00DataSource} isMobile={mobile} />
-    </div>
-  );
-});
+  render() {
+    return (
+      <div>
+        <Header dataSource={Nav00DataSource} isMobile={this.state.isMobile} />
+        {this.props.children}
+        <Footer dataSource={Footer00DataSource} isMobile={this.state.isMobile} />
+      </div>
+    );
+  }
+}
+
+export default Layout;
